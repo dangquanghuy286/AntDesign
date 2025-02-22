2f4626026157d1980af34c84d8f6894967aa630a
dangquanghuy286 <154116366+dangquanghuy286@users.noreply.github.com>
2025-02-23 01:59:05 +0700

    SELECTER

--- src/pages/Room/index.js
+++ src/pages/Room/index.js
@@ -1,20 +1,34 @@
-import { DatePicker, Button, Checkbox, Col, Input, Row, Space, Radio } from 'antd';
+import { DatePicker, Button, Checkbox, Col, Input, Row, Space, Radio, Select } from 'antd';
 
 import "./BookingRoom.css"
 import CartItem from '../../components/CartItem';
 import { useState } from 'react';
 const { RangePicker } = DatePicker;
 function BookRoom() {
-    const [data, setData] = useState({})
+    const [data, setData] = useState({
+        time: "14 giờ"
+    })
+    const options = [];
+    for (let i = 0; i <= 24; i++) {
+        options.push({
+            value: i > 9 ? `${i} giờ` : `0${i} giờ`
+        })
+    }
     const handleChangeInput = (e) => {
         const object = {
             ...data,
             [e.target.name]: e.target.value
         }
         setData(object)
-
-
     }
+    const handleChangeSelecter = (e) => {
+        const object = {
+            ...data,
+            time: e
+        }
+        setData(object)
+    }
+
     const handleChangeCheckbox = (e) => {
 
         const object = {
@@ -68,6 +82,17 @@ function BookRoom() {
                         <p>Địa chỉ</p>
                         <Input name='fullname' placeholder="Điền địa chỉ" onChange={handleChangeInput} />
                     </Col>
+
+                    <Col span={12}>
+                        <h4>Lịch trình
+                        </h4>
+                        <RangePicker placeholder={["Ngày đến:", "Ngày đi:"]} format="DD-MM-YYYY" onChange={handleChangeDatePicker} />
+                    </Col>
+                    <Col span={12}>
+                        <h4>Gio nhan phong
+                        </h4>
+                        <Select placeholder={["Giờ đến:"]} style={{ width: "100%" }} onChange={handleChangeSelecter} options={options} defaultValue={data.time} />
+                    </Col>
                     <Col span={12}>
                         <p>Dịch vụ khác</p>
                         <Checkbox.Group onChange={handleChangeCheckbox}>
@@ -80,16 +105,11 @@ function BookRoom() {
 
                     </Col>
                     <Col span={12}>
-                        <h4>Lịch trình
-                        </h4>
-                        <RangePicker placeholder={["Ngày đến:", "Ngày đi:"]} format="DD-MM-YYYY" onChange={handleChangeDatePicker} />
-                    </Col>
-                    <Col span={12}>
                         <p>Quà tặng</p>
                         <Radio.Group name='gift' onChange={handleChangeInput}>
                             <Space direction="horizontal">
                                 <Radio value="Non_di_bien">Nón đi biển</Radio>
-                                <Radio value="Voucher">Voucher</Radio>
+                                <Radio autoFocus={true} value="Voucher">Voucher</Radio>
                             </Space>
                         </Radio.Group>
 
