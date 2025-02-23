37322f399fac0966749adcb56c1c2b841b73cce2
dangquanghuy286 <154116366+dangquanghuy286@users.noreply.github.com>
2025-02-24 00:43:23 +0700

    Carousef

--- src/components/Grid/index.js
+++ src/components/Grid/index.js
@@ -1,22 +1,24 @@
 import CartItem from "../CartItem";
 import "./LearnGrid.css"
-import { Row, Col }
+import { Row, Col, Carousel }
     from
     "antd"
     ;
+import banner from "../../image/carousef.jpg";
 function HocGrid() {
     return (
         <>
             <Row gutter={[20, 20]}>
                 <Col xxl={6} xl={6} lg={6} md={12} sm={24} xs={24}>
-                    <CartItem title="box 1" /></Col>
+                    <CartItem title="box 1" />
+                </Col>
                 <Col xxl={6} xl={6} lg={6} md={12} sm={24} xs={24}>
                     <CartItem title="box 2" /></Col>
                 <Col xxl={6} xl={6} lg={6} md={12} sm={24} xs={24}>
                     <CartItem title="box 3" /></Col>
                 <Col xxl={6} xl={6} lg={6} md={12} sm={24} xs={24}>
                     <CartItem title="box 4" /></Col>
-            </Row>
+            </Row >
             <Row gutter={[20, 20]} className="mt-20">
                 <Col xxl={16} xl={16} lg={16} md={12} sm={24} xs={24}>
                     <CartItem title="box 5" style={{ height: "400px" }} /></Col>
@@ -37,6 +39,24 @@ function HocGrid() {
                 <Col xxl={8} xl={8} lg={8} md={24} sm={24} xs={24}>
                     <CartItem title="box 11" style={{ height: "400px" }} /></Col>
             </Row>
+            <Carousel>
+                <div className="anh1">
+                    <img src={banner} alt="banner" />
+
+                </div>
+                <div className="anh1">
+                    <img src={banner} alt="banner" />
+
+                </div>
+                <div className="anh1">
+                    <img src={banner} alt="banner" />
+
+                </div>
+                <div className="anh1">
+                    <img src={banner} alt="banner" />
+
+                </div>
+            </Carousel>
 
         </>
     )
