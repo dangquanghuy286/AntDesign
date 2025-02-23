1883da5e59c086dd8c804ab596c13b49e4c2cfb0
dangquanghuy286 <154116366+dangquanghuy286@users.noreply.github.com>
2025-02-24 00:01:34 +0700

    Badge

--- src/pages/ListRoom/index.js
+++ src/pages/ListRoom/index.js
@@ -1,8 +1,10 @@
 import { useEffect, useState } from "react"
 import { getListRoom } from "../../services/serviceRoom";
+import { Badge, Card, Col, Row } from "antd";
 
 function ListRoom() {
     const [room, setRoom] = useState([]);
+
     useEffect(() => {
         const fetchApi = async () => {
             const res = await getListRoom();
@@ -14,7 +16,36 @@ function ListRoom() {
 
     return (
         <>
-            List room
+            <Row gutter={[20, 20]}>
+                {room.map((item) => (
+                    <Col span={12} key={item.id}>
+                        <Badge.Ribbon text={item.roomType?.includes("vip") ? "Phòng VIP" : "Phòng thường"} color={item.roomType?.includes("vip") ? "red" : "green"}>
+                            {/* sử dụng includes() để kiểm tra xem "vip" có trong danh sách đã chọn hay không: */}
+                            <Card title={item.roomName}>
+                                <p>
+                                    Số giường: <strong>
+                                        {item.bedCount}
+                                    </strong>
+                                </p>
+                                <p>
+                                    Số người: <strong>
+                                        {item.maxOccupancy}
+                                    </strong>
+                                </p>
+                                {/* <p>
+                                Loại phòng: <strong>
+                                    
+                                    {item.roomType.map((type) => roomLabels[type]).join(", ")}
+                                </strong>
+                            </p> */}
+                                <p>
+                                    {item.roomStatus ? (<Badge status="success" text="Còn phòng" />) : (<Badge status="error" text="Hết phòng" />)}
+                                </p>
+                            </Card>
+                        </Badge.Ribbon>
+                    </Col>
+                ))}
+            </Row>
         </>
     )
 }
